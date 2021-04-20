0e31dc60bc38b77ed39a2129c8d99c244370264e
nierax <axel.niering@lexasoft.de>
2021-04-20 13:25:46 +0200

    Class for cli application

---
+++ ls-mandelbrot-core/src/main/java/de/lexasoft/mandelbrot/cli/MandelbrotCLI.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package de.lexasoft.mandelbrot.cli;
+
+/**
+ * @author nierax
+ *
+ */
+public class MandelbrotCLI {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
