def4fbb92c0ed607819792bf515b3abf838e915f
nierax <axel.niering@lexasoft.de>
2021-04-21 18:09:32 +0200

    Check added, whether the image could be written to the filesystem

--- ls-mandelbrot-core/src/main/java/de/lexasoft/mandelbrot/MandelbrotImage.java
+++ ls-mandelbrot-core/src/main/java/de/lexasoft/mandelbrot/MandelbrotImage.java
@@ -13,11 +13,13 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 
 /**
- * @author admin
+ * This class represents an image of a Mandelbrot set calculation.
+ * 
+ * @author nierax
  *
  */
 public class MandelbrotImage {
-	
+
 	private BufferedImage image;
 	private Graphics2D g2d;
 
@@ -28,17 +30,36 @@ public class MandelbrotImage {
 		g2d.setColor(Color.WHITE);
 		g2d.fillRect(0, 0, width, height);
 	}
-	
+
+	/**
+	 * Draw a point with the given color.
+	 * 
+	 * @param point The point to draw.
+	 * @param color The color, the point should have.
+	 * @return The color used.
+	 */
 	public Color colorizePoint(Point point, Color color) {
 		g2d.setColor(color);
 		g2d.drawLine(point.x, point.y, point.x, point.y);
 		return color;
 	}
-	
+
+	/**
+	 * Writes the image into a file with the given filename.
+	 * <p>
+	 * The file type (*.tiff, *.png, *.jpg,...) is used to define the type of the
+	 * image.
+	 * 
+	 * @param qualifiedFilename
+	 * @throws IOException
+	 */
 	public void writeAsFile(String qualifiedFilename) throws IOException {
 		File file = new File(qualifiedFilename);
 		String filetype = qualifiedFilename.substring(qualifiedFilename.lastIndexOf(".") + 1);
-		ImageIO.write(image, filetype, file);
+		boolean written = ImageIO.write(image, filetype, file);
+		if (!written) {
+			throw new IOException("Image could not be written to file \"" + qualifiedFilename + "\"");
+		}
 	}
 
 }
