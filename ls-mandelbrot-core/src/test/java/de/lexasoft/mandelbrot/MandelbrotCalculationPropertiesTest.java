4cfea15d0747fa41c56328cc1fb37476e16113f2
nierax <axel.niering@lexasoft.de>
2021-04-22 18:10:13 +0200

    Import of assert method corrected.

--- ls-mandelbrot-core/src/test/java/de/lexasoft/mandelbrot/MandelbrotCalculationPropertiesTest.java
+++ ls-mandelbrot-core/src/test/java/de/lexasoft/mandelbrot/MandelbrotCalculationPropertiesTest.java
@@ -3,8 +3,8 @@
  */
 package de.lexasoft.mandelbrot;
 
-import static org.junit.Assert.assertSame;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
 
 import java.io.IOException;
 
