61b21ee228badfabc9d72b12cba983e19fb9be05
nierax <axel.niering@lexasoft.de>
2021-06-13 14:16:46 +0200

    ControllerModel as interface introduced

---
+++ ls-mandelbrot-swing/src/main/java/de/lexasoft/mandelbrot/swing/model/ControllerModel.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package de.lexasoft.mandelbrot.swing.model;
+
+/**
+ * Interface to be used to access the controller model.
+ * 
+ * @author nierax
+ *
+ */
+public interface ControllerModel {
+
+	/**
+	 * 
+	 * @return The color controller model
+	 */
+	ColorControllerModel colorCM();
+}
