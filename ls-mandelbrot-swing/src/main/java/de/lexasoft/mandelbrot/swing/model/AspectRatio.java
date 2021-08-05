9d1812c1d628f98ad45daae4322e10e2e919b8b9
nierax <axel.niering@lexasoft.de>
2021-08-05 19:08:11 +0200

    Readble entries in aspect ratio pull down

--- ls-mandelbrot-swing/src/main/java/de/lexasoft/mandelbrot/swing/model/AspectRatio.java
+++ ls-mandelbrot-swing/src/main/java/de/lexasoft/mandelbrot/swing/model/AspectRatio.java
@@ -14,37 +14,39 @@ public enum AspectRatio {
 	/**
 	 * Don't care about an aspect ratio
 	 */
-	IGNORE(Double.NaN),
+	IGNORE(Double.NaN, "Ignore"),
 
 	/**
 	 * Use the full area to display the image.
 	 */
-	FILL(0d),
+	FILL(0d, "Fill"),
 
 	/**
 	 * Quadratic
 	 */
-	AR1x1(1d),
+	AR1x1(1d, "1:1"),
 
 	/**
 	 * 3 to 2
 	 */
-	AR3x2(3d / 2d),
+	AR3x2(3d / 2d, "3:2"),
 
 	/**
 	 * 4 to 3
 	 */
-	AR4x3(4d / 3d),
+	AR4x3(4d / 3d, "4:3"),
 
 	/**
 	 * 16 to 10
 	 */
-	AR16x10(16d / 10d),
+	AR16x10(16d / 10d, "16:10"),
 
 	/**
 	 * 16 to 9
 	 */
-	AR16x9(16d / 9d);
+	AR16x9(16d / 9d, "16:9");
+
+	private String outText;
 
 	/**
 	 * @return the ratioX2Y
@@ -53,10 +55,16 @@ public enum AspectRatio {
 		return ratioX2Y;
 	}
 
+	@Override
+	public String toString() {
+		return outText;
+	}
+
 	private Double ratioX2Y;
 
-	private AspectRatio(Double ratioX2Y) {
+	private AspectRatio(Double ratioX2Y, String outText) {
 		this.ratioX2Y = ratioX2Y;
+		this.outText = outText;
 	}
 
 }
