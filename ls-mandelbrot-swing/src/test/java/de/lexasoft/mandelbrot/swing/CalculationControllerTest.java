77682ca279fe03234649900b8aadc7bb7be9408f
nierax <axel.niering@lexasoft.de>
2021-10-17 19:01:56 +0200

    Made calculation controller type safe to text fields.

--- ls-mandelbrot-swing/src/test/java/de/lexasoft/mandelbrot/swing/CalculationControllerTest.java
+++ ls-mandelbrot-swing/src/test/java/de/lexasoft/mandelbrot/swing/CalculationControllerTest.java
@@ -9,10 +9,11 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.text.ParseException;
+
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
-import javax.swing.JTextField;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -20,6 +21,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import de.lexasoft.common.swing.LSJFormattedTextField;
 import de.lexasoft.mandelbrot.api.MandelbrotPointPosition;
 import de.lexasoft.mandelbrot.ctrl.CalculationAttributesDTO;
 import de.lexasoft.mandelbrot.ctrl.MandelbrotAttributesDTO;
@@ -39,15 +41,15 @@ class CalculationControllerTest {
 	@Mock
 	private CalculationPanel view;
 	@Mock
-	private JTextField tlCx;
+	private LSJFormattedTextField<Double> tlCx;
 	@Mock
-	private JTextField tlCy;
+	private LSJFormattedTextField<Double> tlCy;
 	@Mock
-	private JTextField brCx;
+	private LSJFormattedTextField<Double> brCx;
 	@Mock
-	private JTextField brCy;
+	private LSJFormattedTextField<Double> brCy;
 	@Mock
-	private JTextField maxIter;
+	private LSJFormattedTextField<Integer> maxIter;
 	@Mock
 	private JComboBox<AspectRatio> aspectRatio;
 	@Mock
@@ -76,9 +78,11 @@ class CalculationControllerTest {
 	/**
 	 * Test method for
 	 * {@link de.lexasoft.mandelbrot.swing.CalculationController#CalculationController(de.lexasoft.mandelbrot.api.MandelbrotCalculationProperties, de.lexasoft.mandelbrot.swing.CalculationPanel)}.
+	 * 
+	 * @throws ParseException
 	 */
 	@Test
-	final void testCalculationController() {
+	final void testCalculationController() throws ParseException {
 		// Check the result
 		assertNotNull(cut); // Should never fail
 		// Controller model
@@ -87,25 +91,27 @@ class CalculationControllerTest {
 		assertEquals(calc.getBottomRight(), cut.bottomRight());
 		assertEquals(calc.getMaximumIterations(), cut.maximumIterations());
 		// View initialized?
-		verify(tlCx).setText(Double.toString(calc.getTopLeft().cx()));
-		verify(tlCy).setText(Double.toString(calc.getTopLeft().cy()));
-		verify(brCx).setText(Double.toString(calc.getBottomRight().cx()));
-		verify(brCy).setText(Double.toString(calc.getBottomRight().cy()));
-		verify(maxIter).setText(Integer.toString(calc.getMaximumIterations()));
+		verify(tlCx).setBasicValue(calc.getTopLeft().cx());
+		verify(tlCy).setBasicValue(calc.getTopLeft().cy());
+		verify(brCx).setBasicValue(calc.getBottomRight().cx());
+		verify(brCy).setBasicValue(calc.getBottomRight().cy());
+		verify(maxIter).setBasicValue(calc.getMaximumIterations());
 		verify(aspectRatio).setSelectedItem(AspectRatio.FILL);
 	}
 
 	/**
 	 * What happens, when the calculate button is pressed?
+	 * 
+	 * @throws ParseException
 	 */
 	@SuppressWarnings("unchecked")
 	@Test
-	final void testBtnClaculationPressed() {
+	final void testBtnClaculationPressed() throws ParseException {
 		// Simulate input values
-		when(tlCx.getText()).thenReturn("-0.15");
-		when(tlCy.getText()).thenReturn("-0.1");
-		when(brCx.getText()).thenReturn("-0.05");
-		when(brCy.getText()).thenReturn("0");
+		when(tlCx.getBasicValue()).thenReturn(-0.15);
+		when(tlCy.getBasicValue()).thenReturn(-0.1);
+		when(brCx.getBasicValue()).thenReturn(-0.05);
+		when(brCy.getBasicValue()).thenReturn(0d);
 
 		// Initialize
 		cut.initController();
@@ -116,10 +122,10 @@ class CalculationControllerTest {
 
 		// Check
 		// Are values set correctly from view to controller model?
-		verify(tlCx).getText();
-		verify(tlCy).getText();
-		verify(brCx).getText();
-		verify(brCy).getText();
+		verify(tlCx).getBasicValue();
+		verify(tlCy).getBasicValue();
+		verify(brCx).getBasicValue();
+		verify(brCy).getBasicValue();
 		// Have listeners been informed?
 		verify(listener).modelChanged(any(ModelChangedEvent.class));
 		// Check, whether the values in controller model are set correctly
@@ -130,7 +136,7 @@ class CalculationControllerTest {
 	}
 
 	@Test
-	final void testReplaceModel() {
+	final void testReplaceModel() throws ParseException {
 		// Set new values in calculation model
 		CalculationAttributesDTO calc = model.getCalculation();
 		calc.setTopLeft(MandelbrotPointPosition.of(0.6, 0.4));
@@ -141,11 +147,11 @@ class CalculationControllerTest {
 		cut.replaceModel(calc);
 
 		// Then check, whether the values are set correctly.
-		verify(tlCx).setText("0.6");
-		verify(tlCy).setText("0.4");
-		verify(brCx).setText("-0.5");
-		verify(brCy).setText("0.2");
-		verify(maxIter).setText("53");
+		verify(tlCx).setBasicValue(0.6);
+		verify(tlCy).setBasicValue(0.4);
+		verify(brCx).setBasicValue(-0.5);
+		verify(brCy).setBasicValue(0.2);
+		verify(maxIter).setBasicValue(53);
 	}
 
 }
