d36ba959ff4cc5a732e858190fdf98c3cf421216
nierax <axel.niering@lexasoft.de>
2021-10-17 13:14:27 +0200

    PaletteVariant with readable description

--- ls-mandelbrot-core/src/main/java/de/lexasoft/mandelbrot/api/PaletteVariant.java
+++ ls-mandelbrot-core/src/main/java/de/lexasoft/mandelbrot/api/PaletteVariant.java
@@ -18,24 +18,24 @@ public enum PaletteVariant {
 	/**
 	 * Black and White only, grading not possible
 	 */
-	BLACK_WHITE(),
+	BLACK_WHITE("Black and White"),
 
 	/**
 	 * Rainbow in 29 colors
 	 */
-	RAINBOW29(new Color(128, 0, 0), new Color(130, 40, 40), new Color(141, 83, 59), new Color(153, 102, 117),
-	    new Color(153, 102, 169), new Color(128, 0, 128), new Color(101, 0, 155), new Color(72, 0, 225),
-	    new Color(4, 0, 208), new Color(0, 68, 220), new Color(1, 114, 226), new Color(1, 159, 232),
-	    new Color(11, 175, 162), new Color(23, 179, 77), new Color(0, 212, 28), new Color(0, 255, 0),
-	    new Color(128, 255, 0), new Color(200, 255, 0), new Color(255, 255, 0), new Color(255, 219, 0),
-	    new Color(255, 182, 0), new Color(255, 146, 0), new Color(255, 109, 0), new Color(255, 73, 0),
-	    new Color(255, 0, 0), new Color(255, 0, 128), new Color(255, 105, 180), new Color(255, 0, 255),
-	    new Color(168, 0, 185)),
+	RAINBOW29("Rainbow 29 colors", new Color(128, 0, 0), new Color(130, 40, 40), new Color(141, 83, 59),
+	    new Color(153, 102, 117), new Color(153, 102, 169), new Color(128, 0, 128), new Color(101, 0, 155),
+	    new Color(72, 0, 225), new Color(4, 0, 208), new Color(0, 68, 220), new Color(1, 114, 226),
+	    new Color(1, 159, 232), new Color(11, 175, 162), new Color(23, 179, 77), new Color(0, 212, 28),
+	    new Color(0, 255, 0), new Color(128, 255, 0), new Color(200, 255, 0), new Color(255, 255, 0),
+	    new Color(255, 219, 0), new Color(255, 182, 0), new Color(255, 146, 0), new Color(255, 109, 0),
+	    new Color(255, 73, 0), new Color(255, 0, 0), new Color(255, 0, 128), new Color(255, 105, 180),
+	    new Color(255, 0, 255), new Color(168, 0, 185)),
 
 	/**
 	 * Rainbow in 7 colors
 	 */
-	RAINBOW7(Color.RED, Color.ORANGE, Color.YELLOW, Color.GREEN, Color.BLUE,
+	RAINBOW7("Rainbow 7 colors", Color.RED, Color.ORANGE, Color.YELLOW, Color.GREEN, Color.BLUE,
 	    // Indigo
 	    new Color(75, 0, 130),
 	    // Purple
@@ -44,16 +44,18 @@ public enum PaletteVariant {
 	/**
 	 * 2 colors sky blue and white
 	 */
-	BLUEWHITE(new Color(25, 140, 255), Color.WHITE),
+	BLUEWHITE("Blue and White", new Color(25, 140, 255), Color.WHITE),
 
 	/**
 	 * Custom palette, defined by user (f.ex. in YAML file)
 	 */
-	CUSTOM();
+	CUSTOM("Custom palette");
 
 	private List<Color> colorPalette;
+	private String name;
 
-	private PaletteVariant(Color... colors) {
+	private PaletteVariant(String name, Color... colors) {
+		this.name = name;
 		colorPalette = new ArrayList<>();
 		for (Color color : colors) {
 			colorPalette.add(color);
@@ -69,6 +71,11 @@ public enum PaletteVariant {
 
 	public List<Color> colorPalette() {
 		return colorPalette;
+	}
+
+	@Override
+	public String toString() {
+		return name;
 	}
 
 }
