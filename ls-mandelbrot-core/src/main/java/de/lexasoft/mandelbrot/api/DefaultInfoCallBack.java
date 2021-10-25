953df2942f2547629d4b5a2e2b0344e5458dfe30
nierax <axel.niering@lexasoft.de>
2021-10-25 14:52:00 +0200

    Logging duration with java util logging

--- ls-mandelbrot-core/src/main/java/de/lexasoft/mandelbrot/api/DefaultInfoCallBack.java
+++ ls-mandelbrot-core/src/main/java/de/lexasoft/mandelbrot/api/DefaultInfoCallBack.java
@@ -3,6 +3,9 @@
  */
 package de.lexasoft.mandelbrot.api;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * This default implementation of {@link InfoCallbackAPI} writes information to
  * the standard output.
@@ -11,6 +14,8 @@ package de.lexasoft.mandelbrot.api;
  */
 public class DefaultInfoCallBack implements InfoCallbackAPI {
 
+	private static Logger LOGGER = LoggerFactory.getLogger(DefaultInfoCallBack.class);
+
 	@Override
 	public void outFileWritten(String filenameWritten2) {
 		System.out.println("Image file written to: " + filenameWritten2);
@@ -18,7 +23,7 @@ public class DefaultInfoCallBack implements InfoCallbackAPI {
 
 	@Override
 	public void outCalculationReady(long timeUsed) {
-		System.out.println(String.format("Calculation ready. Time used: %s ms.", timeUsed));
+		LOGGER.info((String.format("Calculation ready. Time used: %s ms.", timeUsed)));
 	}
 
 }
