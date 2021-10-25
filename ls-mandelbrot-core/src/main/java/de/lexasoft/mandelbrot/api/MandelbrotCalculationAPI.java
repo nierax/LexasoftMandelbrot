e90d324479d6c7743d2ceaa528b201ee6d88a232
nierax <axel.niering@lexasoft.de>
2021-10-25 12:09:09 +0200

    Calculation api refactored to use CalculationArea and ImageArea

--- ls-mandelbrot-core/src/main/java/de/lexasoft/mandelbrot/api/MandelbrotCalculationAPI.java
+++ ls-mandelbrot-core/src/main/java/de/lexasoft/mandelbrot/api/MandelbrotCalculationAPI.java
@@ -33,8 +33,8 @@ public class MandelbrotCalculationAPI {
 		    model.getColorGrading(), model.getMandelbrotColor());
 		MandelbrotIterator calculator = MandelbrotIterator.of(colorize);
 		long start = System.currentTimeMillis();
-		MandelbrotImage image = calculator.drawMandelbrot(model.getTopLeft(), model.getBottomRight(),
-		    model.getMaximumIterations(), model.getImageWidth(), model.getImageHeight());
+		MandelbrotImage image = calculator.drawMandelbrot(model.getCalculation(), model.getMaximumIterations(),
+		    model.getImage());
 		long stop = System.currentTimeMillis();
 		InfoCallbackAPI.of().outCalculationReady(stop - start);
 		return image;
