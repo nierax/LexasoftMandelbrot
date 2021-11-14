4b428274acbc78186303c81cc2293bb44a4ea266
nierax <axel.niering@lexasoft.de>
2021-11-14 10:44:56 +0100

    Image resizes to the pane it is embedded in.

--- ls-mandelbrot-fx/src/main/java/de/lexasoft/mandelbrot/fx/ImageController.java
+++ ls-mandelbrot-fx/src/main/java/de/lexasoft/mandelbrot/fx/ImageController.java
@@ -7,11 +7,14 @@ import de.lexasoft.mandelbrot.MandelbrotImage;
 import de.lexasoft.mandelbrot.api.AspectRatioHandle;
 import de.lexasoft.mandelbrot.ctrl.MandelbrotAttributesDTO;
 import de.lexasoft.mandelbrot.ctrl.MandelbrotController;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 
 /**
  * 
@@ -21,18 +24,38 @@ import javafx.scene.image.ImageView;
 public class ImageController implements Initializable {
 
 	@FXML
+	private AnchorPane imagePane;
+
+	@FXML
 	private ImageView mbImage;
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		Image image = SwingFXUtils.toFXImage(calculate(AspectRatioHandle.FOLLOW_IMAGE).getImage(), null);
-		mbImage.setImage(image);
+		imagePane.widthProperty().addListener(new ChangeListener<Number>() {
+
+			@Override
+			public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+				resizeImage(AspectRatioHandle.FITIN, newValue, imagePane.getHeight());
+			}
+		});
+
+		imagePane.heightProperty().addListener((v, o, c) -> resizeImage(AspectRatioHandle.FITIN, imagePane.getWidth(), c));
 	}
 
-	private MandelbrotImage calculate(AspectRatioHandle arHandle) {
+	private MandelbrotImage calculate(AspectRatioHandle arHandle, int width, int height) {
 		MandelbrotAttributesDTO attribs = MandelbrotAttributesDTO.ofDefaults();
 		attribs.getImage().setAspectRatioHandle(arHandle);
+		attribs.getImage().setImageHeight(height);
+		attribs.getImage().setImageWidth(width);
 		return MandelbrotController.of().executeSingleCalculation(attribs);
+	}
+
+	private void resizeImage(AspectRatioHandle arHandle, Number width, Number height) {
+		if ((width.intValue() > 0) && (height.intValue() > 0)) {
+			Image image = SwingFXUtils
+			    .toFXImage(calculate(AspectRatioHandle.FITIN, width.intValue(), height.intValue()).getImage(), null);
+			mbImage.setImage(image);
+		}
 	}
 
 }
