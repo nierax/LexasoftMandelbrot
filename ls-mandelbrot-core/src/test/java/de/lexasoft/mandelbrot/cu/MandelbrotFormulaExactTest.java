13f88ee8278e4c15322829e0b93ef1e6f1793c9a
AxelN <axel.niering@lexasoft.de>
2023-11-30 15:09:00 +0100

    Corrected some formations

--- ls-mandelbrot-core/src/test/java/de/lexasoft/mandelbrot/cu/MandelbrotFormulaExactTest.java
+++ ls-mandelbrot-core/src/test/java/de/lexasoft/mandelbrot/cu/MandelbrotFormulaExactTest.java
@@ -3,13 +3,12 @@
  */
 package de.lexasoft.mandelbrot.cu;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.math.BigDecimal;
 import java.util.stream.Stream;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
